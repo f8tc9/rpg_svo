bdb8c152909d20183511ac0f4f34e7ecb45c400a
f8tc9 <125724083+f8tc9@users.noreply.github.com>
2023-03-15 07:34:19 +0800

    Add files via upload

--- 01.py
+++ 01.py
@@ -1,46 +1,80 @@
-player_name = input("请输入玩家名称：")
-player_level = 1
-player_hp = 100
-player_attack = 10
-player_defense = 5
+import random
 
-print(f"欢迎 {player_name} 加入游戏！")
+class Character:
+    def __init__(self, name, health, attack, defense):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
 
-while True:
-    enemy_hp = 50
-    enemy_attack = 8
-    enemy_defense = 3
-    
-    print("你遇到了一只恶魔！")
-    
-    while enemy_hp > 0:
-        player_damage = player_attack - enemy_defense
-        enemy_damage = enemy_attack - player_defense
-        
-        if player_damage > 0:
-            enemy_hp -= player_damage
-            print(f"你攻击了恶魔，恶魔剩余血量为 {enemy_hp} 点！")
+    def attack_enemy(self, enemy):
+        damage = self.attack - enemy.defense
+        if damage > 0:
+            enemy.health -= damage
+            print(f"{self.name} attacks {enemy.name} and deals {damage} damage!")
         else:
-            print("你的攻击被恶魔防御了！")
-        
-        if enemy_hp <= 0:
-            print("你击败了恶魔！")
-            player_level += 1
-            player_hp += 20
-            player_attack += 5
-            player_defense += 2
-            print(f"你升级了！你的等级提升到了 {player_level} 级！")
-            print(f"你的生命值提升到了 {player_hp} 点！")
-            print(f"你的攻击力提升到了 {player_attack} 点！")
-            print(f"你的防御力提升到了 {player_defense} 点！")
-            break
-        
-        if enemy_damage > 0:
-            player_hp -= enemy_damage
-            print(f"恶魔攻击了你，你的剩余生命值为 {player_hp} 点！")
-        else:
-            print("恶魔攻击被你防御了！")
-        
-        if player_hp <= 0:
-            print("你被恶魔击败了，游戏结束！")
-            exit()
+            print(f"{self.name}'s attack is too weak to damage {enemy.name}!")
+
+    def is_alive(self):
+        return self.health > 0
+
+class Player(Character):
+    def __init__(self, name, health, attack, defense):
+        super().__init__(name, health, attack, defense)
+        self.experience = 0
+        self.level = 1
+
+    def gain_experience(self, experience):
+        self.experience += experience
+        print(f"{self.name} gains {experience} experience points!")
+        if self.experience >= self.level * 10:
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        self.health += 5
+        self.attack += 2
+        self.defense += 2
+        self.experience = 0
+        print(f"{self.name} levels up to level {self.level}!")
+
+class Enemy(Character):
+    def __init__(self, name, health, attack, defense, experience):
+        super().__init__(name, health, attack, defense)
+        self.experience = experience
+
+    def drop_loot(self):
+        loot_table = {
+            "gold": random.randint(0, 10),
+            "potion": random.randint(0, 1),
+            "sword": random.randint(0, 1)
+        }
+        print(f"{self.name} drops:")
+        for item, quantity in loot_table.items():
+            if quantity > 0:
+                print(f"{quantity} {item}")
+        return loot_table
+
+def fight(player, enemy):
+    print(f"{player.name} (Level {player.level}, Health {player.health}) vs {enemy.name} (Experience {enemy.experience}, Health {enemy.health})")
+    while player.is_alive() and enemy.is_alive():
+        player.attack_enemy(enemy)
+        if enemy.is_alive():
+            enemy.attack_enemy(player)
+    if player.is_alive():
+        player.gain_experience(enemy.experience)
+        loot_table = enemy.drop_loot()
+        return loot_table
+    else:
+        print(f"{player.name} has been defeated!")
+        return None
+
+# Example usage
+player = Player("Bob", 20, 5, 5)
+goblin = Enemy("Goblin", 10, 3, 1, 5)
+
+loot = fight(player, goblin)
+if loot:
+    print("Loot obtained:")
+    for item, quantity in loot.items():
+        print(f"{quantity} {item}")
